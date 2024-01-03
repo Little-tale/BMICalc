781aaf6bebb08b15f10e523abc945a6862d4fe33
Little-tale <you3192001@gmail.com>
2024-01-03 19:05:29 +0900

    Initial Commit

---
+++ BMIAppAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMIAppAssignment
+//
+//  Created by Jae hyung Kim on 1/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
